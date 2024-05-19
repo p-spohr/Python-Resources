b2e8366e01185d44cd8b78e54276fbf3822bdd5a
Patrick <spohrpatrick@gmail.com>
2024-05-19 20:28:29 +0200

    add VS Code navigation introduction

---
+++ Navigation.py
@@ -0,0 +1,106 @@
+#### Navigating Visual Code Studio ####
+## never touch your mouse again!
+
+
+# use # to create a comment
+# this is a comment
+
+
+# use ''' at beginning and ''' at end to create doc string
+'''
+This is a docstring used to describe code and will show up in hints
+It can be multiline and can be used for variables or functions
+'''
+
+x = 'variable x'
+'''
+describe the variable so the next user or future you will know what it does
+'''
+y = 'variable y'
+'''
+describe the variable so the next user or future you will know what it does
+'''
+z = 'variable z'
+'''
+describe the variable so the next user or future you will know what it does
+'''
+
+
+# CTRL + SPACE => open suggestions
+# while in suggestions: CTRL + UP/DOWN ARROW => move up and down in suggestions
+# while suggestion selected: TAB => select suggestion
+# ESC => to exit suggestion
+x # move cursor to end of variable and press hotkey
+y
+z 
+
+
+# CTRL + ALT + D => comment
+# CTRL + ALT + E => uncomment
+# uncomment this comment
+# uncomment this comment
+# uncomment this comment
+
+
+# CTRL + P => search files by name
+# CTRL + K & CTRL + S => open keyboard shortcuts
+
+
+# ALT + Up/Down Arrow => move line up/down
+# move the numbers back in ascending order
+# 1
+# 4 
+# 3
+# 2
+
+
+# ALT + LEFT/RIGHT ARROW => switch tabs
+
+
+# ALT + F/E/S/V/G/R/T/H => open top menu item according to initial
+
+
+# CTRL + SHIFT + F => open search bar explorer
+# CTRL + SHIFT + E => open file explorer
+
+
+# CTRL + UP/DOWN ARROW => scroll up or down
+# CTRL + LEFT/RIGHT ARROW => skip white spaces and word to next one                   
+
+
+# CTRL + SHIFT + RIGHT/LEFT/UP/DOWN ARROW => highlight in pressed direction
+
+
+# ALT + LEFT MOUSE => add new cursor at clicked position
+
+
+# CTRL + ALT + UP/DOWN ARROW => add new cursor in pressed direction
+# write duplicate code quickly
+# write duplicate code quickly
+# write duplicate code quickly
+# write duplicate code quickly
+
+
+# CTRL + D => highlight next selected text
+# change the name from Tally to Kenly
+# Hi! My name is Tally and I like to code!
+# Hi! My name is Tally and I like to code!
+# Hi! My name is Tally and I like to code!
+
+
+# CTRL + SHIFT + S => save all
+
+# ALT + F4 => close window (Windows command)
+
+
+
+
+
+
+
+
+
+
+
+
+
