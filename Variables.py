39943a1b1bbf6995bff6e1e0e28c8a4144cbcc60
Patrick <spohrpatrick@gmail.com>
2024-05-21 18:35:16 +0200

    update file structure

---
+++ Variables.py
@@ -0,0 +1,23 @@
+# Variables
+
+x = 'variable x'
+'''
+describe the variable so the next user or future you will know what it does
+'''
+y = 'variable y'
+'''
+describe the variable so the next user or future you will know what it does
+'''
+z = 'variable z'
+'''
+describe the variable so the next user or future you will know what it does
+'''
+
+
+# CTRL + SPACE => open suggestions
+# while in suggestions: CTRL + UP/DOWN ARROW => move up and down in suggestions
+# while suggestion selected: TAB => select suggestion
+# ESC => to exit suggestion
+x # move cursor to end of variable and press CTRL + SPACE
+y  
+z
