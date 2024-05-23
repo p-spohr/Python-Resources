b1a320aa7be3c2cfaba1660c052cfb0a8aedef07
Patrick <spohrpatrick@gmail.com>
2024-05-23 11:05:24 +0200

    create bat files to run simple commands

--- test_command.bat
+++
@@ -1,6 +0,0 @@
-
-@echo off
-
-Rem This is for listing down all the files in the directory Program files 
-dir "C:\Users\pat_h\OneDrive\public-repos\Python-Resources" > C:\Users\pat_h\OneDrive\public-repos\Python-Resources\dir.txt 
-echo "The program has completed"
