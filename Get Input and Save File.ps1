f7313f060eb012aa017d3387827d31e4f370ce88
Patrick <spohrpatrick@gmail.com>
2024-06-19 21:57:59 +0200

    PowerShell CLI input and file saving

---
+++ Get Input and Save File.ps1
@@ -0,0 +1,8 @@
+﻿$FirstName = Read-Host -Prompt "First Name"
+$LastName = Read-Host -Prompt "Last Name"
+
+$Year = (Get-Date).Year 
+$Month = (Get-Date).Month
+$Day = (Get-Date).Day
+
+New-Item -Value "$FirstName $LastName" -ItemType File -Path ".\$Year-$Month-$Day $FirstName $LastName.txt"
