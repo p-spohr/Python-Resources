656ba8c6837e127bea78f374b96a518cef1d6634
Patrick <spohrpatrick@gmail.com>
2025-03-09 13:31:33 +0100

    encoding tests

--- CCTA/chinese_character_print.py
+++ CCTA/chinese_character_print.py
@@ -29,3 +29,34 @@ with open('chinese_test_ledger.csv', encoding='UTF-8') as chn_test:
 print(ledger.split('\n'))
 
 # %%
+
+import re
+# %%
+
+m = re.match(r'<tr>(.*)</tr>', '<tr>hi</tr>')
+print(m.group(1))
+print(m.groups())
+# %%
+html_path = 'C:\\Users\\pat_h\\OneDrive\\p-spohr-repos\\Python-Resources\\CCTA\\encoding_raw.html'
+with open(html_path) as html_file:
+    raw_html = html_file.read()
+# %%
+all_encodings = []
+for line in raw_html.split('\n'):
+    # print(line)
+    try:
+        m = re.search(r'<tr class="row-\w*"><td><p>(.*)</p></td>', line)
+        print(m.groups())
+        all_encodings.append(m.group(1))
+    except AttributeError:
+        continue
+# %%
+all_encodings
+
+# %%
+
+with open('all_encodings.txt', mode='w') as file:
+    for item in all_encodings:
+        file.write(f'{str(item)}\n')
+
+# %%
