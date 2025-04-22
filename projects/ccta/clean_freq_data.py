27f74f0df463f058e1e8caa4a0abc43da67c6fef
Patrick <spohrpatrick@gmail.com>
2025-04-22 22:15:56 +0200

    finish cleaning character csv files and finish anki deck

--- projects/ccta/clean_freq_data.py
+++ projects/ccta/clean_freq_data.py
@@ -23,28 +23,50 @@ mcf_df = pd.read_table(os.path.join(path_to_file, 'modern_character_freq.txt'),
 
 # %%
 
-mcf_df.head()
-
+mcf_df
 
 # %%
 
-mcf_df = mcf_df.loc[mcf_df.eng.notna()]
+mcf_df.loc[mcf_df.hanzi == '啰']
+
+# %%
+
+replace_character = hex(ord('�'))
+
+# %%
+
+mcf_df.hanzi
+
+# %%
+
+def replace_error(data_ser : pd.Series, rep_char : str):
+  char_list = list(data_ser)
+  error_check = False
+  for char in char_list:
+    if hex(ord(char)) == rep_char:
+      error_check = True
+
+  return error_check
+
+# %%
+
+error_ser = mcf_df.hanzi.apply(replace_error, args= (replace_character,))
+
+# %%
+
+mcf_df = mcf_df.assign(error=error_ser)
+
+# %%
+
+mcf_df = mcf_df.loc[mcf_df.error != True]
 
 # %%
 
 mcf_df = mcf_df.reset_index(drop=True)
 
-
 # %%
 
 mcf_df
-
-# %%
-
-replace_character = '0xFFD'
-print(chr(int(replace_character, base=16)))
-
-test_error_character = chr(int(replace_character, base=16))
 
 # %%
 
@@ -60,27 +82,35 @@ trad_hanzi = pd.read_csv('traditional_hanzi.csv',
 
 # %%
 
-# has NA values
-trad_hanzi.isna().sum()
+combined_df = pd.DataFrame({'simplified': mcf_df.hanzi,
+              'traditional': trad_hanzi.traditional})
+
+# %%
+
+combined_df.loc[combined_df.traditional.isna()]
 
 # %%
 
 # get index where NA
-idx = trad_hanzi.loc[trad_hanzi.traditional.isna()].index
+idx = combined_df.loc[combined_df.traditional.isna()].index
 
 # %%
 
 # fill in missing values at index
-missing_charaters = ['孀', '菀', '碭', '貲', '籀','駘']
-trad_hanzi.loc[idx, 'traditional'] = missing_charaters
+missing_charaters = ['孀', '菀', '碭', np.nan, '貲', '籀','駘', np.nan]
+combined_df.loc[idx, 'traditional'] = missing_charaters
 
 # %%
 
-trad_hanzi.to_csv('traditional_hanzi_1.csv', header= False, index= False, encoding= 'utf-8')
+combined_df = combined_df.loc[combined_df.traditional.notna()]
 
 # %%
 
-mcf_df.insert(1, 'traditional', trad_hanzi)
+mcf_df = mcf_df.rename(columns= {'hanzi': 'simplified'})
+
+# %%
+
+mcf_df = mcf_df.merge(combined_df, how='left', on='simplified')
 
 # %%
 
@@ -92,7 +122,18 @@ mcf_df.rename(columns= {'hanzi':'simplified'}, inplace=True)
 
 # %%
 
-mcf_df
+mcf_df = mcf_df.loc[mcf_df.traditional.notna()]
+
+# %%
+
+mcf_df = mcf_df[['simplified', 'traditional', 'pinyin', 'eng', 'freq']]
+
+# %%
+
+mcf_df.to_csv('modern_characters_freq_cleaned.csv',
+              encoding= 'utf-8',
+              index= False,
+              sep= '\t')
 
 # %%
 
@@ -163,118 +204,6 @@ ccdict_df = pd.read_table(os.path.join(path_to_vocab, 'cc-cedict.u8'),
                           encoding='utf-8',
                           delimiter='\t',
                           nrows=5)
-
-# %%
-
-import re
-
-# %%
-
-ccre = re.compile(r'\[(.*)\]')
-
-res = ccre.search('䰾魚 鲃鱼 [ba1 yu2] /barbel (fish)/')
-
-print(res)
-
-# %%
-
-ccre = re.compile(r'\[(.*)\]')
-
-res = ccre.search('㐜 㐜 [chou2] /variant of 仇[chou2]/')
-
-print(res)
-
-
-
-# %%
-
-ccre = re.compile(r'(.+)\s(.+)\s\[(.+)\]\s/(.+)/')
-
-res = ccre.search('11區 11区 [Shi2 yi1 Qu1] /(ACG) Japan (from the anime "Code Geass", in which Japan was renamed Area 11)/test/test/')
-
-print(res.groups())
-
-# %%
-
-for m in res.groups():
-    print(m)
-
-# %%
-
-new_df = pd.DataFrame(columns=['traditional', 'simplified', 'pinyin', 'eng'])
-
-# %%
-
-s_group = pd.Series(res.groups())
-s_group.index = new_df.columns
-s_group.to_frame().T
-
-# %%
-
-pd.concat([new_df, s_group.to_frame().T], axis= 0, ignore_index=True)
-
-# %%
-
-new_df.loc[0] = s_group
-
-# %%
-new_df
-# %%
-
-with open(os.path.join(path_to_vocab, 'cc-cedict.u8'), encoding='utf-8', mode='r') as file:
-    ccdict = file.read()
-    
-# %%
-
-cclist = ccdict.split('\n')
-
-# %%
-
-ccprep = [line for line in cclist if line[0] != '#']
-
-# %%
-
-ccre = re.compile(r'(.+)\s(.+)\s\[(.+)\]\s/(.+)/')
-
-res = ccre.search(ccprep[101])
-
-print(res.groups())
-
-# %%
-
-ccfinish = []
-for entry in ccprep:
-    result = ccre.search(entry)
-    ccfinish.append(result.groups())
-    
-# %%
-
-cc_df = pd.DataFrame(ccfinish, columns= ['traditional', 'simplified', 'pinyin', 'eng'])
-
-# %%
-
-cc_df.isna().apply(sum, 0)
-
-# %%
-
-hsk_df = pd.read_csv('chinese_vocabulary_lists/words/hsk1-6.csv', sep='\t', usecols= [0,2,3,4])
-hsk_df
-
-# %%
-
-hsk_df.shape
-
-# %%
-
-inter_hsk_cc = np.intersect1d(cc_df.simplified, hsk_df.Hanzi)
-
-# %%
-
-diff_hsk_cc = np.setdiff1d(hsk_df.Hanzi, inter_hsk_cc)
-
-# %%
-
-hsk_df.loc[hsk_df.Hanzi.isin(diff_hsk_cc)]
 
 # %%
 
